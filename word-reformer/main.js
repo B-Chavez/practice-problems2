cbee25f1770d2285b0a2bc7cd0b99e6adc097a27
b-chavez <brandedhard@gmail.com>
2020-09-12 11:32:14 -0700

    Added JS to make HTML

--- word-reformer/main.js
+++ word-reformer/main.js
@@ -1,3 +1,14 @@
+var div = document.createElement("div");
+div.textContent = "Display Full Words Here:";
+document.body.appendChild(div);
+
+var divDisplay = document.createElement("div");
+divDisplay.setAttribute("class", "display");
+document.body.appendChild(divDisplay);
+
+var display = document.querySelector(".display");
+
 function wordReformer(word1, word2){
+    display.textContent += word1 + word2 + " "
     return word1 + word2;
 }
